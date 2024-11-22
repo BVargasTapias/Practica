03591db267c525ceccaac295aca4d2a108854c72
BVargasTapias <brayanvargas0710@gmail.com>
2024-11-22 13:08:41 -0500

    Actualizaciones en las pag

---
+++ front/js/administrador.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnUsuarios = document.getElementById("btnUsuarios")
+    const containerUsuarios = document.getElementById("containerUsuarios")
+})
